5539593b53232cdae92c7de4db22e0a3ef353a2b
Indika Wijesooriya <indika@arimaclanka.com>
2020-10-06 18:42:19 +0530

    First Build
    This is the first build of Colour Balls game

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1601989480,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/menu-sheet0.png",
+		"images/triangle-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/ball-sheet1.png",
+		"images/ball-sheet2.png",
+		"images/text_score.png",
+		"images/particle1.png",
+		"images/particle2.png",
+		"images/particle3.png",
+		"images/shaker-sheet0.png",
+		"media/click.ogg",
+		"media/piont.ogg",
+		"media/thud.ogg",
+		"media/woosh.ogg",
+		"media/back.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
